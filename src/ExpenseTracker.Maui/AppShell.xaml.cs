1c1283d2eb1455f3714476215f73303aa2a8761f
Jowell Victor Cano <70183520+jvgcano@users.noreply.github.com>
2026-08-09 12:12:56 +0800

    feat: establish application architecture

---
+++ src/ExpenseTracker.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTracker.Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/ExpenseTracker.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ExpenseTracker.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
